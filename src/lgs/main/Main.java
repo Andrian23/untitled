593497cdbde349478b5daeb27fcb0914b84fd428
Andrian23 <andriansarabrin23@gmail.com>
2016-07-10 21:08:51 +0300

    add sout Andrian

--- src/lgs/main/Main.java
+++ src/lgs/main/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Uraazaaaasdsaf");
         System.out.println("dsfsdfsdfsd");
+        System.out.println("Andrian");
     }
 }
