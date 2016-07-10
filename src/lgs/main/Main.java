522a10e0eaaff96f842d80903d4c84af9e08c790
Andrian23 <andriansarabrin23@gmail.com>
2016-07-10 21:04:53 +0300

    Initial commit

---
+++ src/lgs/main/Main.java
@@ -0,0 +1,10 @@
+package lgs.main;
+
+/**
+ * Created by andri on 10.07.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Uraazaaaasdsaf");
+    }
+}
